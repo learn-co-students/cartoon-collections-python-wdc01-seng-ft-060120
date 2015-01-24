0c87a73ca1f073356859e03067d1609fae2f8f9b
Joe Burgess <joe@flatironschool.com>
2015-01-24 07:54:01 -0500

    Adding stuff

---
+++ solution.py
@@ -0,0 +1,17 @@
+dwarves = ["Larry","Curly","Moe"]
+
+print(roll_call_dwarves(dwarves))
+
+forces = ["Earth","Wind","fire"]
+print(summon_captain_planet(forces))
+
+print(long_planeteer_calls(forces))
+
+potential_cheese = ["James","cheddar", "CAMbert", "Guido", "flatiron"]
+
+print (find_the_cheese(potential_cheese))
+
+receipt = ["$$", "$", "$$$"]
+
+print (calculate_dollar_amount(receipt))
+
