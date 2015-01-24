0242f027fdcead30383d7ab1e582e4ddcd23a141
Joe Burgess <joe@flatironschool.com>
2015-01-24 07:54:26 -0500

    Adding solution

--- solution.py
+++ solution.py
@@ -1,3 +1,26 @@
+def roll_call_dwarves(dwarves):
+    result = ""
+    for index,dwarf in enumerate(dwarves):
+        result += "{}. {}".format(index+1,dwarf)
+        if index != len(dwarves)-1:
+            result+= " | "
+    return result
+
+def summon_captain_planet(forces):
+    return [force.upper()+"!" for force in forces]
+
+def long_planeteer_calls(planteer_calls):
+    return [call for call in planteer_calls if len(call)>4]
+
+def find_the_cheese(potentially_cheesy_items):
+    cheeses = ["cheddar", "gouda", "cambert"]
+
+    return [cheese for cheese in potentially_cheesy_items
+            if cheese.lower() in cheeses]
+
+def calculate_dollar_amount(receipt):
+    return [str(item.count("$")) for item in receipt]
+
 dwarves = ["Larry","Curly","Moe"]
 
 print(roll_call_dwarves(dwarves))
