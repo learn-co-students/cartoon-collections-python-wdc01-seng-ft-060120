b46d7147feab5398a41384d9dddacddc1ae26770
Robert Cobb <rwcobbjr@gmail.com>
2015-05-26 00:19:06 -0400

    getting into some interesting territory

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,17 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves dwarf_array
+  dwarf_array.each_with_index {|name,i| puts "#{i + 1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet calls_array
+  calls_array.map{|e| e.capitalize + "!"}
 end
 
-def long_planteer_calls# code an argument here
-  # Your code here
+def long_planteer_calls calls
+  !calls.select{|e| e.length > 4 }.empty?
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese list
   cheese_types = ["cheddar", "gouda", "camembert"]
+  i = list.index{|e| cheese_types.include?(e) }
+  i ? list[i] : nil
 end
